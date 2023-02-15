3fe118a007c4e1c8366ba8b73fe50e6e514c8f42
AliAhmad06 <aliaurangzaib05@gmail.com>
2023-02-15 15:55:01 +0500

    Feb 15

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ const postRoutes = require('./routes/post')
 const morgan = require('morgan') //middleware
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const expressValidator = require('express-validator')
 
 const app = express()
 dotenv.config()
 
 mongoose.set('strictQuery', false)
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }).then(() => console.log('DB Connected'))
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .then(() => console.log('DB Connected'))
 mongoose.connection.on('error', (err) => {
   console.log(`DB connection error: ${err.message}`)
 })
@@ -21,6 +25,8 @@ const myOwnMiddleWare = (req, res, next) => {
 }
 
 app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(expressValidator())
 app.use(myOwnMiddleWare)
 
 app.use('/', postRoutes)
--- controllers/post.js
+++ controllers/post.js
@@ -1,11 +1,30 @@
+const Post = require('../models/post')
+
 exports.getPosts = (req, res) => {
   //res.send('Hello world from Node JS routes folder')
   res.json({
-      posts: [
-          { title: 'First post' },
-          { title: 'Second Post' }
-      ],
+    posts: [{ title: 'First post' }, { title: 'Second Post' }],
   })
 }
 
 //module.exports = { getPosts }
+
+exports.createPost = (req, res) => {
+  const post = new Post(req.body)
+  console.log('Creating Post: ', req.body)
+  // post.save((err, result) => {
+  //   if (err) {
+  //     return res.status(400).json({
+  //       error: err
+  //     })
+  //   }
+  //   res.status(200).json({
+  //     post: result
+  //   })
+  // })
+  post.save().then((result) => {
+    res.status(200).json({
+      post: result,
+    })
+  })
+}
